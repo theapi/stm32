8b3bd1defc7baa4537528fb45e7f8f365407992a
theapi <petermclarke@gmail.com>
2016-10-20 20:31:31 +0100

    Read the button with the HAL library

--- NUCLEO_L053R8/Button_HAL/src/main.c
+++ NUCLEO_L053R8/Button_HAL/src/main.c
@@ -5,14 +5,25 @@
 #include "stm32l0xx.h"
 #include "stm32l0xx_nucleo.h"
 
+GPIO_InitTypeDef GPIO_InitStructure;
 int main(void) {
 
 	HAL_Init();
 	BSP_LED_Init(LED2);
 
+	// Initialise the button (PC13)
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	GPIO_InitStructure.Pin = GPIO_PIN_13;
+	GPIO_InitStructure.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStructure.Pull = GPIO_NOPULL; // External pull up
+	GPIO_InitStructure.Speed = GPIO_SPEED_LOW;
+	HAL_GPIO_Init(GPIOC, &GPIO_InitStructure);
+
 	while (1) {
-		BSP_LED_Toggle(LED2);
-		HAL_Delay(500);
+		if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13)) {
+			BSP_LED_Toggle(LED2);
+			//HAL_Delay(500);
+		}
 	}
 
 }
