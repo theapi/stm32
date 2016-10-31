8d58f533e56134a320b53bf355fa7347040d7d73
theapi <petermclarke@gmail.com>
2016-10-31 19:30:11 +0000

    Output the clock to MCO (pin PA8)

---
+++ NUCLEO_L053R8/RCCtoMCO/src/main.c
@@ -0,0 +1,26 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32l0xx.h"
+#include "stm32l0xx_nucleo.h"
+			
+
+int main(void)
+{
+	HAL_Init();
+
+	// Output the clock to PA8
+	HAL_RCC_MCOConfig(RCC_MCO1, RCC_MCO1SOURCE_SYSCLK, RCC_MCODIV_1);
+
+	while(1) {
+
+	}
+}
