b1ffcddd4a444115d60bbd9942cbaf71c2af3c16
theapi <petermclarke@gmail.com>
2016-11-19 18:43:45 +0000

    LCD driver on larger 64 pin chip

---
+++ NUCLEO_L053R8/NUCLEO_L053R8_LCD_S301C31TR/Src/lcd.c
@@ -0,0 +1,204 @@
+/**
+  ******************************************************************************
+  * File Name          : LCD.c
+  * Description        : This file provides code for the configuration
+  *                      of the LCD instances.
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2016 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "lcd.h"
+
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+LCD_HandleTypeDef hlcd;
+
+/* LCD init function */
+void MX_LCD_Init(void)
+{
+
+  hlcd.Instance = LCD;
+  hlcd.Init.Prescaler = LCD_PRESCALER_16;
+  hlcd.Init.Divider = LCD_DIVIDER_25;
+  hlcd.Init.Duty = LCD_DUTY_STATIC;
+  hlcd.Init.Bias = LCD_BIAS_1_4;
+  hlcd.Init.VoltageSource = LCD_VOLTAGESOURCE_INTERNAL;
+  hlcd.Init.Contrast = LCD_CONTRASTLEVEL_5;
+  hlcd.Init.DeadTime = LCD_DEADTIME_0;
+  hlcd.Init.PulseOnDuration = LCD_PULSEONDURATION_0;
+  hlcd.Init.HighDrive = LCD_HIGHDRIVE_0;
+  hlcd.Init.BlinkMode = LCD_BLINKMODE_OFF;
+  hlcd.Init.BlinkFrequency = LCD_BLINKFREQUENCY_DIV8;
+  hlcd.Init.MuxSegment = LCD_MUXSEGMENT_DISABLE;
+  if (HAL_LCD_Init(&hlcd) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+
+void HAL_LCD_MspInit(LCD_HandleTypeDef* lcdHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+  if(lcdHandle->Instance==LCD)
+  {
+  /* USER CODE BEGIN LCD_MspInit 0 */
+
+  /* USER CODE END LCD_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_LCD_CLK_ENABLE();
+  
+    /**LCD GPIO Configuration    
+    PC0     ------> LCD_SEG18
+    PC1     ------> LCD_SEG19
+    PC2     ------> LCD_SEG20
+    PC3     ------> LCD_SEG21
+    PC4     ------> LCD_SEG22
+    PC5     ------> LCD_SEG23
+    PB0     ------> LCD_SEG5
+    PB1     ------> LCD_SEG6
+    PB10     ------> LCD_SEG10
+    PB11     ------> LCD_SEG11
+    PB12     ------> LCD_SEG12
+    PB13     ------> LCD_SEG13
+    PB14     ------> LCD_SEG14
+    PB15     ------> LCD_SEG15
+    PC6     ------> LCD_SEG24
+    PC7     ------> LCD_SEG25
+    PC8     ------> LCD_SEG26
+    PC9     ------> LCD_SEG27
+    PA8     ------> LCD_COM0
+    PA15     ------> LCD_SEG17
+    PB3     ------> LCD_SEG7
+    PB4     ------> LCD_SEG8
+    PB5     ------> LCD_SEG9
+    PB8     ------> LCD_SEG16 
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3 
+                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7 
+                          |GPIO_PIN_8|GPIO_PIN_9;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF1_LCD;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_10|GPIO_PIN_11 
+                          |GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15 
+                          |GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_8;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF1_LCD;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_15;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF1_LCD;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN LCD_MspInit 1 */
+
+  /* USER CODE END LCD_MspInit 1 */
+  }
+}
+
+void HAL_LCD_MspDeInit(LCD_HandleTypeDef* lcdHandle)
+{
+
+  if(lcdHandle->Instance==LCD)
+  {
+  /* USER CODE BEGIN LCD_MspDeInit 0 */
+
+  /* USER CODE END LCD_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_LCD_CLK_DISABLE();
+  
+    /**LCD GPIO Configuration    
+    PC0     ------> LCD_SEG18
+    PC1     ------> LCD_SEG19
+    PC2     ------> LCD_SEG20
+    PC3     ------> LCD_SEG21
+    PC4     ------> LCD_SEG22
+    PC5     ------> LCD_SEG23
+    PB0     ------> LCD_SEG5
+    PB1     ------> LCD_SEG6
+    PB10     ------> LCD_SEG10
+    PB11     ------> LCD_SEG11
+    PB12     ------> LCD_SEG12
+    PB13     ------> LCD_SEG13
+    PB14     ------> LCD_SEG14
+    PB15     ------> LCD_SEG15
+    PC6     ------> LCD_SEG24
+    PC7     ------> LCD_SEG25
+    PC8     ------> LCD_SEG26
+    PC9     ------> LCD_SEG27
+    PA8     ------> LCD_COM0
+    PA15     ------> LCD_SEG17
+    PB3     ------> LCD_SEG7
+    PB4     ------> LCD_SEG8
+    PB5     ------> LCD_SEG9
+    PB8     ------> LCD_SEG16 
+    */
+    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3 
+                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7 
+                          |GPIO_PIN_8|GPIO_PIN_9);
+
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_10|GPIO_PIN_11 
+                          |GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15 
+                          |GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_8);
+
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_8|GPIO_PIN_15);
+
+  }
+  /* USER CODE BEGIN LCD_MspDeInit 1 */
+
+  /* USER CODE END LCD_MspDeInit 1 */
+} 
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
