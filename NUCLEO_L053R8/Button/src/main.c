cb336fd5c691e97fef70ab682ed525f1edc000b3
theapi <petermclarke@gmail.com>
2016-11-27 12:45:16 +0000

    Experimenting with registry access

--- NUCLEO_L053R8/Button/src/main.c
+++ NUCLEO_L053R8/Button/src/main.c
@@ -5,6 +5,23 @@
 #include "stm32l0xx.h"
 #include "stm32l0xx_nucleo.h"
 
+/* A memory structure that starts at GPIOA_BASE ((uint32_t)0x40000000U + 0x10000000U) */
+typedef struct
+{
+    __IO uint32_t MODER;        /*!< GPIO port mode register,                     Address offset: 0x00 */
+    __IO uint32_t OTYPER;       /*!< GPIO port output type register,              Address offset: 0x04 */
+    __IO uint32_t OSPEEDR;      /*!< GPIO port output speed register,             Address offset: 0x08 */
+    __IO uint32_t PUPDR;        /*!< GPIO port pull-up/pull-down register,        Address offset: 0x0C */
+    __IO uint32_t IDR;          /*!< GPIO port input data register,               Address offset: 0x10 */
+    __IO uint32_t ODR;          /*!< GPIO port output data register,              Address offset: 0x14 */
+    __IO uint32_t BSRR;         /*!< GPIO port bit set/reset registerBSRR,        Address offset: 0x18 */
+    __IO uint32_t LCKR;         /*!< GPIO port configuration lock register,       Address offset: 0x1C */
+    __IO uint32_t AFR[2];       /*!< GPIO alternate function register,            Address offset: 0x20-0x24 */
+    __IO uint32_t BRR;          /*!< GPIO bit reset register,                     Address offset: 0x28 */
+}FOO_TypeDef;
+#define FOO               ((FOO_TypeDef *) GPIOA_BASE)
+
+
 /**
  * Initialise the on board LED: PORT A, PIN 5
  */
@@ -43,14 +60,21 @@ int
 main (void)
 {
 
+
+
   // Configure the on board LED.
   led_init ();
 
   // Configure the on board button.
   button_init ();
 
+  FOO->BSRR |= GPIO_BSRR_BS_5;
+  //GPIOA->BSRR |= GPIO_BSRR_BS_5;
+
   while (1)
     {
+
+
 
       // Read the button.
       if (GPIOC->IDR && GPIO_IDR_ID13_Msk)
